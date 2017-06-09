1d4c6dd4cc547642e51d3ec831897b7312cab74f
Michaël Perrin <michael.perrin84@gmail.com>
2017-06-09 12:10:32 +0200

    Fix compatibility with Sphinx 1.6+

--- sensio/sphinx/bestpractice.py
+++ sensio/sphinx/bestpractice.py
@@ -1,6 +1,12 @@
 from docutils.parsers.rst import Directive, directives
 from docutils import nodes
-from sphinx.util.compat import make_admonition
+
+# DeprecationWarning: make_admonition is deprecated, use docutils.parsers.rst.directives.admonitions.BaseAdmonition instead
+try:
+    from sphinx.util.compat import make_admonition
+except ImportError:
+    from docutils.parsers.rst.directives.admonitions import BaseAdmonition as make_admonition
+
 from sphinx import addnodes
 from sphinx.locale import _
 
